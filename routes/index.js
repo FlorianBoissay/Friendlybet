e9d735001b33c226eb02e278639b61950bf43e28
Florian BOISSAY <florianboissay@yahoo.fr>
2018-04-23 15:31:42 +0200

    Initialisation des routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/miser', function(req, res, next) {
+  res.render('miser')
+});
+
+router.get('/inscription', function(req, res, next) {
+  res.render('inscription')
+});
+
+router.get('/connexion', function(req, res, next) {
+  res.render('connexion')
+});
+
 module.exports = router;
