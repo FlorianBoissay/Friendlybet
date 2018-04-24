29c56e1bbe50888956c7c231c66296a5664f670f
Florian BOISSAY <florianboissay@yahoo.fr>
2018-04-24 10:45:15 +0200

    Finalisation d'intégration envoi photo

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,21 @@ router.get('/connexion', function(req, res, next) {
 router.get('/envoiphoto', function(req, res, next) {
   res.render('envoiphoto')
 });
+
+router.post('/upload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('/florian/lacapsule/friendlybet/betapp/exemple.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
 module.exports = router;
